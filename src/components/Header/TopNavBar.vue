c64aa6b5c0e3e13b62ad36696ec29089b663415e
hamzashiwani <hm4465496@gmail.com>
2025-05-12 23:44:48 +0500

    frontend fixes

--- src/components/Header/TopNavBar.vue
+++ src/components/Header/TopNavBar.vue
@@ -3,12 +3,29 @@
         <div class="ant-col ant-col-10 css-i6rspj">
             <ul class="ant-menu-overflow ant-menu ant-menu-root ant-menu-horizontal ant-menu-light nav-menu-custom css-i6rspj" role="menu" tabindex="0" data-menu-list="true" style="width: 100%;">
                 <li class="ant-menu-overflow-item ant-menu-submenu ant-menu-submenu-horizontal" role="none" style="opacity: 1; order: 0;">
-                    <div role="menuitem" class="ant-menu-submenu-title" tabindex="-1" aria-expanded="false" aria-haspopup="true" data-menu-id="rc-menu-uuid-26089-1-/categories" aria-controls="rc-menu-uuid-26089-1-/categories-popup">
-                        <img src="/assets/image/cart-right-icon.webp" alt="Cart Right Icon" class="ant-menu-item-icon" style="width: 10px; rotate: -90deg; filter: brightness(0) invert(1);">
-                        <span class="ant-menu-title-content">
-                            <span>Shop By Category</span>
-                        </span>
-                        <i class="ant-menu-submenu-arrow"></i>
+                    <div class="dropdown-container">
+                            <button class="dropdown-btn">Shop By Category</button>
+                            <ul class="dropdown-menu">
+                              <li class="dropdown-item">
+                                Storage Devices
+                                <ul class="submenu">
+                                  <li class="dropdown-item">
+                                    Hard Drives
+                                    <ul class="submenu">
+                                      <li class="dropdown-item">Server Hard Drives</li>
+                                      <li class="dropdown-item">Desktop Hard Drives</li>
+                                      <li class="dropdown-item">Laptop Hard Drives</li>
+                                    </ul>
+                                  </li>
+                                  <li class="dropdown-item">Solid State Drives</li>
+                                  <li class="dropdown-item">USB Flash Drives</li>
+                                </ul>
+                              </li>
+                              <li class="dropdown-item">Memory</li>
+                              <li class="dropdown-item">Network & Accessories</li>
+                              <li class="dropdown-item">PC & Servers</li>
+                              <li class="dropdown-item">Softwares</li>
+                            </ul>
                     </div>
                 </li>
                 <li class="ant-menu-overflow-item ant-menu-overflow-item-rest ant-menu-submenu ant-menu-submenu-horizontal ant-menu-submenu-disabled" aria-hidden="true" role="none" style="opacity: 0; height: 0px; overflow-y: hidden; order: 2147483647; pointer-events: none; position: absolute;">
@@ -47,3 +64,73 @@
         settings: Object
     })
 </script>
+<style>
+      .dropdown-container {
+      position: relative;
+      display: inline-block;
+    }
+
+    .dropdown-btn {
+      /* background-color: #003366; */
+      color: #fff;
+      padding: 12px 18px;
+      font-size: 16px;
+      font-weight: bold;
+      border: none;
+      cursor: pointer;
+    }
+
+    .dropdown-menu,
+    .submenu {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      position: absolute;
+      display: none;
+      background-color: #ffffff;
+      min-width: 220px;
+      border: 1px solid #ccc;
+      z-index: 999;
+      /* box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); */
+    }
+
+    .dropdown-menu {
+      top: 100%;
+      left: 0;
+    }
+
+    .submenu {
+      top: 0;
+      left: 100%;
+    }
+
+    .dropdown-item {
+      padding: 5px 5px;
+      cursor: pointer;
+      white-space: nowrap;
+      position: relative;
+      background-color: #ffffff;
+      color: #333;
+      transition: background-color 0.3s ease;
+    }
+
+    .dropdown-item:hover {
+      background-color: #f5f5f5;
+    }
+
+    .dropdown-item:hover > .submenu {
+      display: block;
+    }
+
+    .dropdown-container:hover .dropdown-menu {
+      display: block;
+    }
+
+    .dropdown-item:has(.submenu)::after {
+      content: '>';
+      position: absolute;
+      right: 12px;
+      color: #000000;
+      font-size: 12px;
+    }
+    </style>
