32729b456f6019f2c3227c56991641650425d801
hamzashiwani <hm4465496@gmail.com>
2025-05-29 20:07:21 +0500

    some changes

--- src/components/Home/BestSellingProduct.vue
+++ src/components/Home/BestSellingProduct.vue
@@ -45,13 +45,13 @@
                         <img src="/assets/logos/payment-shipping-icon1.png" alt="Security Icon">
                     </div>
                     <h4> All Secure Payment Methods </h4>
-                    <img src="/assets/logos/payment-options.png" class="payment_shipping_img" alt="Secure Payment Methods Icon">
+                    <img src="/assets/logos/payment-options.webp" class="payment_shipping_img" alt="Secure Payment Methods Icon">
                 </div>
             </div>
             <div class="col-sm-4">
                 <div style="padding:34px !important;" class="payment_shipping_box payment_shipping_box_center">
                     <div class="icon_p">
-                        <img src="/assets/logos/payment-shipping-icon2.png" alt="Happy Face Icon">
+                        <img src="/assets/logos/payment-shipping-icon2.webp" alt="Happy Face Icon">
                     </div>
                     <h4> Satisfaction Guaranteed </h4>
                     <h5> Easy return within 30 days, <br> <span> no question asked! </span> </h5>
@@ -60,10 +60,10 @@
             <div class="col-sm-4">
                 <div style="padding:30px !important;" class="payment_shipping_box">
                     <div class="icon_p">
-                        <img src="/assets/logos/payment-shipping-icon3.png" alt="Delivery Truck Icon">
+                        <img src="/assets/logos/payment-shipping-icon3.webp" alt="Delivery Truck Icon">
                     </div>
                     <h4> Free Ground Shipping in US </h4>
-                    <img src="/assets/logos/fedex.png" class="payment_shipping_img" alt="Fedex Logo">
+                    <img src="/assets/logos/fedex.webp" class="payment_shipping_img" alt="Fedex Logo">
                 </div>
             </div>
         </div>
