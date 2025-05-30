97d36c1ab1c01b9fadc0e78803063c9fb571a874
hamzashiwani <hm4465496@gmail.com>
2025-05-31 02:39:47 +0500

    some changes

--- src/components/Header/Search.vue
+++ src/components/Header/Search.vue
@@ -3,14 +3,14 @@
     <span class="ant-input-group-wrapper ant-input-search ant-input-group-wrapper-outlined css-i6rspj ant-input-search-with-button header-search-input">
       <span class="ant-input-wrapper ant-input-group css-i6rspj">
         <input
-          v-model="searchTerm"
-          @input="handleSearch"
-          @keydown.enter="handleSearch"
-          placeholder="What can we help you find today?"
-          class="ant-input ant-input-outlined"
-          type="text"
-          style="padding-left: 12px"
-        />
+        v-model="searchTerm"
+        @input="handleSearch"
+        @keydown.enter="handleSearch2"
+        placeholder="What can we help you find today?"
+        class="ant-input ant-input-outlined"
+        type="text"
+        style="padding-left: 12px"
+      />
         <span class="ant-input-group-addon">
           <button type="button" @click="handleSearch2" class="ant-btn ant-btn-primary ant-btn css-i6rspj ant-btn-primary ant-btn-color-primary ant-btn-variant-solid ant-input-search-button">
             <img src="/assets/image/search-icon.webp" alt="Search Icon" style="width: 15px; filter: brightness(0) invert(1);" />
